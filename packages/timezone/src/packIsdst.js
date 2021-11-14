36b451b461f77b35c44c7806a1e89c3d153dc6b6
Philipp Thuerwaechter <pithu@thuerwaechter.de>
2021-11-14 18:55:19 +0100

    base64 2 state approach

--- packages/timezone/src/packIsdst.js
+++ packages/timezone/src/packIsdst.js
@@ -11,63 +11,71 @@ export const b64ToUint6 = (char) => {
     } else if (nChr === 47 ) {
         return 63;
     }
-    return 0;
+    return 65;
 };
 
 export const uint6ToB64 = (nUint6) => {
-    let char = 65;
+    let charCode = 65;
     if(nUint6 < 26) {
-        char = nUint6 + 65;
+        charCode = nUint6 + 65;
     } else if (nUint6 < 52) {
-        char = nUint6 + 71;
+        charCode = nUint6 + 71;
     } else if (nUint6 < 62) {
-        char = nUint6 - 4;
+        charCode = nUint6 - 4;
     } else if (nUint6 === 62) {
-        char = 43;
+        charCode = 43;
     } else if (nUint6 === 63) {
-        char = 47;
+        charCode = 47;
     }
-    return String.fromCharCode(char);
+    return String.fromCharCode(charCode);
 };
 
+const bit6ToB64 = (bit6String) =>
+    bit6String.split('').reduce((acc, bit, idx) => acc + (bit << (5-idx)), 0);
+
+const nToChar = (n) => String.fromCharCode(n + 58);
+const charToN = (char) => char.charCodeAt(0) - 58;
 
 export function packIsdst(intArray = []) {
-    const threeStateArray = intArray.map((isdst) => Math.sign(isdst) + 1);
-    const numberOfBytes = Math.ceil(threeStateArray.length / 3);
-    let packed = '';
-    for (let i = 0; i < numberOfBytes; i += 1) {
-        const idx = i * 3;
-        let uInt6 = 0;
-        for (let j = 0; j < 3; j += 1) {
-            const v = threeStateArray[idx + j];
-            const vm = v == null ? 3 : v;
-            const vs = vm << ((2-j) * 2);
-            uInt6 += vs;
-        }
-        packed += uint6ToB64(uInt6);
-    }
-    return packed;
+    const twoStateArray = intArray.map((isdst) => isdst <= 0 ? 0 : 1);
+    const bit6Chunks = twoStateArray.join('').match(/.{1,6}/g);
+    return  bit6Chunks.reduce((acc, bit6Chunk) => {
+        const nChar = bit6Chunk.length === 6 ? '' : nToChar(bit6Chunk.length);
+        const uint6 = bit6ToB64(bit6Chunk.padEnd(6, '0'));
+        return `${acc}${nChar}${uint6ToB64(uint6)}`;
+    }, '');
 }
 
 const masks = [
-    parseInt('110000', 2),
-    parseInt('001100', 2),
-    parseInt('000011', 2),
+    parseInt('100000', 2),
+    parseInt('010000', 2),
+    parseInt('001000', 2),
+    parseInt('000100', 2),
+    parseInt('000010', 2),
+    parseInt('000001', 2),
 ];
 
-export function unPackIsdst(packedBas64) {
-    const u6 = packedBas64
-        .split('')
-        .map(b64ToUint6);
-    const threeStateArray = [];
-    for (const uInt6 of u6) {
-        for (let j = 0; j < 3; j += 1) {
-            const vs = uInt6 & masks[j];
-            const isdst = vs >> ((2-j)*2);
-            if (isdst !== 3) {
-                threeStateArray.push(isdst);
-            }
-        }
+const uInt6ToBits = (uInt6, n) => {
+    const bits = [];
+    for (let j = 0; j < n; j += 1) {
+        bits.push(uInt6 & masks[j] ? 1 : 0);
     }
-    return threeStateArray.map((isdst) => isdst - 1);
+    return bits;
+};
+
+export function unPackIsdst(packedBase64) {
+    const chars = packedBase64.split('');
+    const unPacked = chars.reduce((acc, b64) => {
+        const uInt6 = b64ToUint6(b64);
+        return (uInt6 > 64) ?
+            {
+                isdst: acc.isdst,
+                n: charToN(b64),
+            } :
+            {
+                isdst: acc.isdst.concat(uInt6ToBits(uInt6, acc.n)),
+                n: 6,
+            };
+    }, { isdst: [], n: 6 });
+    return unPacked.isdst;
 }
